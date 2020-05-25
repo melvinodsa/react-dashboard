a824df8bc799e9637c32aef4112a2986e4ce4509
Melvin Davis <melvinodsa@gmail.com>
2020-05-25 21:10:00 +0530

    path error fix

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     <div className="App">
       <Router>
         <Switch>
-          <Route exact path="/">
+          <Route path="/">
             <Home />
           </Route>
         </Switch>
