c3f95d0b66f4427cac057c41eb70d580a45f71c3
Melvin Davis <melvinodsa@gmail.com>
2020-05-25 20:49:18 +0530

    Updates

---
+++ precache-manifest.2f72b45f4365e55771f0c12737083788.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4067042e614c0a88db5ed67527daf258",
+    "url": "/react-dashboard/index.html"
+  },
+  {
+    "revision": "e34d93121665d39401ce",
+    "url": "/react-dashboard/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "d1f66734c36d11a7fe12",
+    "url": "/react-dashboard/static/js/2.afa7aeaf.chunk.js"
+  },
+  {
+    "revision": "2eb049c4e05d87008457beac921e56da",
+    "url": "/react-dashboard/static/js/2.afa7aeaf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e34d93121665d39401ce",
+    "url": "/react-dashboard/static/js/main.e1951631.chunk.js"
+  },
+  {
+    "revision": "2a879c37702df82b7742",
+    "url": "/react-dashboard/static/js/runtime-main.12fe4569.js"
+  }
+]);
